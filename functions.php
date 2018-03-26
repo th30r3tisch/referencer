a03009cbe4c82f6a0283adb84a47327c3ef5d6a5
th30r3tisch <theoretisch@hotmail.de>
2018-03-26 21:05:13 +0200

    basic templates and bootstrap 4

---
+++ functions.php
@@ -0,0 +1,32 @@
+<?php
+/**
+ * Adds features and functions to the site.
+ *
+ * @package WordPress
+ * @subpackage Referencer	
+ * @since Referencer 0.1.0
+ */
+
+
+/**---------------------------------------------------------------------------
+* adding scripts
+*---------------------------------------------------------------------------**/
+
+// Add scripts and stylesheets (wp_enqueue = instantly loaded || wp_register_ = registered and can be enqued later in the code)
+function frontend_scripts() {
+	wp_enqueue_style( 'bootstrap', get_template_directory_uri() . '/bootstrap-4.0.0/css/bootstrap.min.css', array(), '4.0.0', 'all');
+	wp_enqueue_script( 'bootstrap', get_template_directory_uri() . '/bootstrap-4.0.0/js/bootstrap.min.js', array('jquery'), '4.0.0', true );
+	wp_enqueue_script( 'mainJS', get_template_directory_uri() . '/js/main.js', array('jquery'));
+	wp_enqueue_style( 'mainCSS', get_template_directory_uri() . '/css/main.css' );
+}
+add_action( 'wp_enqueue_scripts', 'frontend_scripts' );
+
+// Add stylesheets for the admin area
+function admin_scripts(){
+	//Core media script
+	wp_enqueue_media();
+	
+	wp_enqueue_script( 'adminJS', get_template_directory_uri() . '/js/admin.js');
+	wp_enqueue_style( 'adminCSS', get_template_directory_uri() . '/css/admin.css' );
+}
+add_action( 'admin_enqueue_scripts', 'admin_scripts' );
