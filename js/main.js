e4d48c7da1cbe1b8e9c25dfef407fc051ba448d5
th30r3tisch <theoretisch@hotmail.de>
2018-04-06 22:33:05 +0200

    improved animation (animate to css transition)

--- js/main.js
+++ js/main.js
@@ -6,16 +6,17 @@ var ajaxElementID = "ajax-content";
 
 jQuery( window ).resize(function() {
 	if(jQuery('.visible-menu').length === 0){
-  		jQuery('#' + ajaxElementID).css("width", parseInt(jQuery(window).width()) - 30 + 'px');
+  		jQuery('#' + ajaxElementID).parent().css("width", getWindowWidth() + 'px');
 	}else{
-		jQuery('#' + ajaxElementID).css("width", parseInt(jQuery(window).width()) - (jQuery('#menu > ul').children('li').length * 30) + 'px');
+		jQuery('#' + ajaxElementID).parent().css("width", getWindowWidth() - (jQuery('#menu > ul').children('li').length * 30) + 30 + 'px');
 	}
 });
+
 
 jQuery( document ).ready(function() {
 	"use strict";
 	
-	// click on burger menu
+	// click on menu close button
 	jQuery('#menuCloseBtn').click(function(){
 		closeContent();
 	});
@@ -24,27 +25,18 @@ jQuery( document ).ready(function() {
 	jQuery('.menu button').click(function(){
 		if(jQuery('.visible-menu').length === 0){ // if menu is not visible
 			burgerToCross();
-			
 			if(contentIsVisible()){ // content is displayed
-				jQuery('#menu').animate({ // fades in the menu
-				left: 0
-				}, 500);
+				fadeInMenu();
 				var menuItemNum = jQuery('#menu > ul').children('li').length;
-				var windowWidth = parseInt(jQuery(window).width());
-				jQuery("#ajax-content").animate({ // shrinks the content container
-					width: windowWidth - (menuItemNum * 30) + 'px'
-					}, 500);
+				jQuery('#' + ajaxElementID).parent().css("width", getWindowWidth() - (menuItemNum * 30) + 30 + 'px'); // shrinks the content container
 				
 			}else{ // no content is visible
 				openMenu();
 			}
-			
 		}else{ // if menu is visible
 			crossToBurger();
-			
 			if(contentIsVisible()){ // content is displayed
-				openContentAnimation(jQuery("#" + ajaxElementID));
-				
+				openContentAnimation(jQuery("#" + ajaxElementID).parent());
 			}else{// no content is visible
 				closeMenu(); 
 			}
@@ -56,11 +48,10 @@ jQuery( document ).ready(function() {
 	jQuery('#menu a.nav-link, #menu a.dropdown-item').click(function(e){
 		if(!jQuery(this).parent().hasClass('menu-item-has-children')){
 			jQuery('.entry-content-page').empty();
-			//window.location(window.location.hostname + "/wordpress/");
 			e.preventDefault();
 			var pageUrl = jQuery(this).attr('href');
 			var pageName = jQuery(this).attr('href').match(/[^/]+(?=\/$|$)/);
-			switchPage(pageName,this);
+			switchPage(pageName,jQuery(this).closest('li'));
 			window.history.pushState(pageName, null, pageUrl);
 		}else{
 			e.preventDefault();
@@ -71,7 +62,7 @@ jQuery( document ).ready(function() {
 
 function switchPage(pageName, obj){
 	if(!contentIsVisible()){
-		openMenu();
+		openMenu(); // just relevant for history
 		crossToBurger();
 		openContentAnimation(obj);
 		flag = pageName;
@@ -80,14 +71,9 @@ function switchPage(pageName, obj){
 		if(flag.toString() !== pageName.toString()){
 			crossToBurger();
 			flag = pageName;
-			jQuery("#" + ajaxElementID).empty();
-			jQuery("#" + ajaxElementID).animate({ // shrinks the content container
-				width: '0px'
-				}, 500, function(){
-				jQuery("#" + ajaxElementID).remove(); // removes the content container
-				openContentAnimation(obj);
-				ajaxCall(pageName, obj); // inserts the content
-			});
+			removeContent();
+			openContentAnimation(obj);
+			ajaxCall(pageName, obj); // inserts the content
 		}
 	}
 }
@@ -103,7 +89,7 @@ function ajaxCall(pageName, obj){
 		context: obj,
 		success:function(data) { // inserst the content in the div inserted before
 				jQuery('#ajax-content').html(data);
-				jQuery('#ajax-content').delay(300).animate({
+				jQuery('#ajax-content').delay(500).animate({
 					opacity: '1'
 					}, 500);
 		},
@@ -116,15 +102,11 @@ function ajaxCall(pageName, obj){
 // animates the appearing pages
 function openContentAnimation(obj){
 	"use strict";
-	var windowWidth = parseInt(jQuery(window).width());
 	jQuery('#menu').animate({
 		left: -30 * nextMenuItemPosition(obj)
-		}, 500);
-	jQuery('#' + ajaxElementID).animate({
-		width: windowWidth - 30 + 'px'
-		}, 500, function(){
-			showBackBtn();
-		});
+		}, 250);
+	jQuery(obj).css("width", "100vw");
+	showBackBtn();
 }
 
 //walks through the history
@@ -135,7 +117,7 @@ window.addEventListener('popstate', function(e){
     if (page === null) {
 		closeContent();
     } else {
-		var obj = jQuery("#menu li a:contains('" + page + "')");
+		var obj = jQuery("#menu li a:contains('" + page + "')").parent();
 		switchPage(page, obj);
     }
 });
@@ -145,15 +127,8 @@ window.addEventListener('popstate', function(e){
 function closeContent(){
 	burgerToCross();
 	hideBackBtn();
-	jQuery("#" + ajaxElementID).empty(); // removes the content
-	jQuery('#menu').animate({ // fades in the menu
-		left: 0
-		}, 500);
-	jQuery("#" + ajaxElementID).animate({ // shrinks the content container
-		width: '0px'
-		}, 500, function(){
-		jQuery("#" + ajaxElementID).remove(); // removes the content container
-	});
+	fadeInMenu();
+	removeContent();
 }
 // hides the button to close the menu
 function hideBackBtn(){
@@ -171,9 +146,10 @@ jQuery.expr[':'].contains = function(a, i, m) {
 function contentIsVisible(){
 	return jQuery("#" + ajaxElementID).length;
 }
+
  // animates appearing menu
 function openMenu(){
-	jQuery('.nav-link').addClass('collapse-side').delay(400).queue(function(next){ // animates appearing menu
+	jQuery('.menu-item').addClass('collapse-side').delay(400).queue(function(next){ // animates appearing menu
 		jQuery('.nav-link').addClass('fade-text');
 		next();
 	});
@@ -181,11 +157,12 @@ function openMenu(){
 // animates closing menu
 function closeMenu(){
 	jQuery('.nav-link').removeClass('fade-text');
-	jQuery('.nav-link').removeClass('collapse-side').delay(400).queue(function(next){
+	jQuery('.menu-item').removeClass('collapse-side').delay(400).queue(function(next){
 		jQuery('.nav-link').removeClass('fade-text');
 		next();
 	});
 }
+
 // changes burger menu to cross
 function burgerToCross(){
 	jQuery('.menu button').addClass('visible-menu'); 
@@ -194,12 +171,25 @@ function burgerToCross(){
 function crossToBurger(){
 	jQuery('.menu button').removeClass('visible-menu');
 }
+
 //returns the next menu item position depenting on the element parameter
 function nextMenuItemPosition(obj){
-	var closestLi = jQuery(obj).closest('li');
 	if(!contentIsVisible()){
-		jQuery(closestLi).append('<div id="' + ajaxElementID + '"></div>');
+		jQuery(obj).append('<div id="' + ajaxElementID + '"></div>');
 	}
-	var elementPositionLeft = jQuery(closestLi).index(); // element position in the menu from the left
+	var elementPositionLeft = jQuery(obj).index(); // element position in the menu from the left
 	return elementPositionLeft;
+}
+//gets the width of the window
+function getWindowWidth(){
+	return parseInt(jQuery(window).width());
+}
+// fade in the menu
+function fadeInMenu(){
+	jQuery('#menu').animate({ left: 0 }, 500); // fades in the menu
+}
+// content remove animation
+function removeContent(){
+	jQuery("#" + ajaxElementID).parent().css("width", "30px"); // shrinks the content container
+	jQuery("#" + ajaxElementID).remove(); // removes the content container
 }
